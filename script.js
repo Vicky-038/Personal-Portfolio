38e944eacb63a28c3d538444637fe195ffc15d25
Vicky-038 <163325823+Vicky-038@users.noreply.github.com>
2024-03-20 18:35:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ var typed = new Typed('#element', {
   strings: ['Web Developer', 'Programmer'],
   typeSpeed: 50,
 });
+
+document.getElementById('myForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent the form from submitting normally
+  var formData = new FormData(this); // Create a FormData object to send form data
+
+  // Send form data to the server using AJAX
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'form.php', true);
+  xhr.onload = function () {
+      if (xhr.status === 200) {
+          // On successful submission, display success message
+          document.getElementById('message').innerHTML = '<div style="color: green;">Message  submitted successfully.</div>';
+          // Clear form fields
+          document.getElementById('myForm').reset();
+          // Hide the success message after 3 seconds
+          setTimeout(function () {
+              document.getElementById('message').innerHTML = '';
+          }, 3000);
+      } else {
+          // On error, display error message
+          document.getElementById('message').innerHTML = '<div style="color: red;">Error occurred. Please try again later.</div>';
+      }
+  };
+  xhr.send(formData); // Send form data
+});
